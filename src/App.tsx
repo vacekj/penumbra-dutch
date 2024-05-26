3e2a4b2d9c01456453b48271e9d98b445e747cde
Atris <vacekj@outlook.com>
2024-05-26 16:40:13 +0200

    feat: main form doneˆ

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import {
 	Box,
+	Button,
 	Container,
 	Flex,
 	FormControl,
 	FormHelperText,
 	FormLabel,
 	Heading,
-	Input, InputGroup, InputRightAddon, InputRightElement,
+	Input,
+	InputGroup,
+	InputRightAddon,
+	InputRightElement,
 	Select,
 	SimpleGrid,
+	Slider,
+	SliderFilledTrack,
+	SliderMark,
+	SliderThumb,
+	SliderTrack, useToast,
 	VStack,
 } from "@chakra-ui/react";
 
@@ -27,10 +36,21 @@ function App() {
 		keyof typeof ASSET_MAP | ""
 	>("");
 
-	const assetText = assetToReceive ? ASSET_MAP[assetToReceive] : "asset";
+	/** Duration of the sum of auctions in seconds, defaults to one hour */
+	const [durationSeconds, setDurationSeconds] = useState(60 * 60);
 
+	const labelStyles = {
+		mt: "2",
+		ml: "-2.5",
+		fontSize: "sm",
+	};
+
+	const [isSubmitting, setIsSubmitting] = useState(false);
+
+	const assetText = assetToReceive ? ASSET_MAP[assetToReceive] : "asset";
+	const toast = useToast();
 	return (
-		<Container maxW={"4xl"} mt={30} mx={"auto"}>
+		<Container maxW={"4xl"} mt={30} mx={"auto"} mb={10}>
 			<Heading
 				fontFamily={"Faktum"}
 				fontWeight={"bold"}
@@ -69,7 +89,7 @@ function App() {
 				mt={10}
 				mx={"auto"}
 				maxW={"xl"}
-				p={6}
+				p={8}
 				rounded={"xl"}
 				bg={"gray.900"}
 				flexDirection={"column"}
@@ -82,7 +102,7 @@ function App() {
 						placeholder="Select token"
 						onChange={(e) => {
 							/*NB: we don't validate the e.target.value as the values are populated from the ASSET_MAP itself.*/
-							setAssetToReceive(e.target.value as keyof typeof ASSET_MAP);
+							setAssetToSell(e.target.value as keyof typeof ASSET_MAP);
 							console.log(e.target.value);
 						}}
 					>
@@ -96,7 +116,12 @@ function App() {
 
 				<FormControl>
 					<FormLabel>Amount to sell</FormLabel>
-					<Input defaultValue={"0"} type={"number"} />
+					<InputGroup>
+						<Input defaultValue={"0"} type={"number"} />
+						<InputRightElement mr={2}>
+							{assetToSell.toUpperCase()}
+						</InputRightElement>
+					</InputGroup>
 				</FormControl>
 
 				<FormControl>
@@ -109,9 +134,9 @@ function App() {
 							console.log(e.target.value);
 						}}
 					>
-						{Object.keys(ASSET_MAP).map((key) => (
-							<option key={key} value={key}>
-								{ASSET_MAP[key as keyof typeof ASSET_MAP]}
+						{Object.keys(ASSET_MAP).map((assetKey) => (
+							<option key={assetKey} value={assetKey}>
+								{ASSET_MAP[assetKey as keyof typeof ASSET_MAP]}
 							</option>
 						))}
 					</Select>
@@ -121,7 +146,9 @@ function App() {
 					<FormLabel>Starting price</FormLabel>
 					<InputGroup>
 						<Input defaultValue={"0"} type={"number"} />
-						<InputRightElement>{assetToReceive.toUpperCase()}</InputRightElement>
+						<InputRightElement mr={2}>
+							{assetToReceive.toUpperCase()}
+						</InputRightElement>
 					</InputGroup>
 					<FormHelperText>
 						Price at which your {assetText} is going to be sold
@@ -132,11 +159,80 @@ function App() {
 					<FormLabel>Reserve price</FormLabel>
 					<InputGroup>
 						<Input defaultValue={"0"} type={"number"} />
-						<InputRightElement>{assetToReceive.toUpperCase()}</InputRightElement>
+						<InputRightElement mr={2}>
+							{assetToReceive.toUpperCase()}
+						</InputRightElement>
 					</InputGroup>
 					<FormHelperText>
 						The lowest price at which your {assetText} is going to be sold
 					</FormHelperText>
+				</FormControl>
+
+				<FormControl pb={10}>
+					<FormLabel>Auction duration</FormLabel>
+					<Slider
+						step={12.5}
+						aria-label="slider-ex"
+						onChange={(val) => setDurationSeconds(val)}
+					>
+						<SliderMark value={0} {...labelStyles}>
+							10 min
+						</SliderMark>
+						<SliderMark value={12.5} {...labelStyles}>
+							30 min
+						</SliderMark>
+						<SliderMark value={25} {...labelStyles}>
+							1 hr
+						</SliderMark>
+						<SliderMark value={37.5} {...labelStyles}>
+							2 hr
+						</SliderMark>
+						<SliderMark value={50} {...labelStyles}>
+							6 hr
+						</SliderMark>
+						<SliderMark value={62.5} {...labelStyles}>
+							12 hr
+						</SliderMark>
+						<SliderMark value={75} {...labelStyles}>
+							24 hr
+						</SliderMark>
+						<SliderMark value={87.5} {...labelStyles}>
+							48 hr
+						</SliderMark>
+						<SliderMark w={"40"} value={100} {...labelStyles}>
+							96 hr
+						</SliderMark>
+
+						<SliderTrack>
+							<SliderFilledTrack />
+						</SliderTrack>
+						<SliderThumb />
+					</Slider>
+				</FormControl>
+
+				<FormControl>
+					<Button
+						isLoading={isSubmitting}
+						loadingText={"Starting auction..."}
+						w={"full"}
+						colorScheme={"blue"}
+						onClick={() => {
+							console.log("starting auction");
+							setIsSubmitting(true);
+							setTimeout(() => {
+								setIsSubmitting(false);
+								toast({
+									title: "Auction created",
+									description: "We've queded an auction to start in 5 minutes",
+									status: "success",
+									duration: 3_000,
+									isClosable: true,
+								})
+							}, 2_000);
+						}}
+					>
+						Create auction
+					</Button>
 				</FormControl>
 			</Flex>
 		</Container>
